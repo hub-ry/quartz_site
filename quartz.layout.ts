cdc6f488d11bfc797d23f48ffc6d3de4460ff0be
hub-ry <rhubbart@purdue.edu>
2025-12-27 01:23:10 -0500

    Quartz sync: Dec 27, 2025, 1:23 AM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -9,6 +9,7 @@ export const sharedPageComponents: SharedLayout = {
   footer: Component.Footer({
     links: {
       GitHub: "https://github.com/hub-ry/quartz_site",
+      LandingPage: "ryhub.dev",
     },
   }),
 }
